5808127e6d8260ed4dfd7e592f958875515dae3f
pvhung05 <phamvanhung242005@gmail.com>
2024-11-30 12:07:48 +0700

    ok:

--- Backend/src/main/java/com/example/demo/repository/FoodRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Food;  // Food model class
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FoodRepository extends JpaRepository<Food, Long> {
-    // Custom queries can be defined here if needed
-}
